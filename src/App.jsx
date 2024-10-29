6d8a2011809874d5d2fb0613689350fa192d73f5
Hamedi-Saboor <saboorhamedi52@gmail.com>
2024-10-29 12:15:06 +0700

    =add tailwind and style button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-
+import "./index.css";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="card ">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="p-2 bg-blue-400 rounded-lg m-3 hover:bg-blue-900 transition-colors ease-in-out transform duration-300 hover:text-white border border-blue-400 border-b-white"
+        >
           count is {count}
         </button>
       </div>
